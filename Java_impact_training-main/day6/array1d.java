603cc67bac03824febbc2deeb724cc428f140e12
Abhi <abhiisoniisonii@gamil.com>
2025-05-10 14:25:28 +0530

    done

---
+++ Java_impact_training-main/day6/array1d.java
@@ -0,0 +1,12 @@
+package day6;
+import java.util.*;
+public class array1d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+         
+           
+
+    }
+    
+}
